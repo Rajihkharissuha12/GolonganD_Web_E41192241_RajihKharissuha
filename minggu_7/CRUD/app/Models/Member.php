c3847946badafb7a670882971ac529f3b6935771
rajhk <72001863+Rajihkharissuha12@users.noreply.github.com>
2021-04-29 02:30:51 +0700

    CRUD

---
+++ minggu_7/CRUD/app/Models/Member.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name','email','phone_number','status'];
+    protected $appednds = ['status_label'];
+
+    public function getStatusLabelAttribute()
+    {
+         if($this->status == 0) {
+             return '<span class="text-red-500">Free</span>';
+         }
+         return '<span class="text-teal-500">Pro</span>';
+    }
+
+
+}
